45d6f2b661733f6ee7e85b3f38c4b40db21a6e62
masanori yoshizuka <yoshizuka@yahoo.co.jp>
2024-02-13 14:28:21 +0900

    mod realtime.

--- detectface/detectFaces.py
+++ detectface/detectFaces.py
@@ -5,11 +5,12 @@ import cv2
 import math
 import numpy as np
 from scipy.spatial.transform import Rotation
+import time
 
 HAAR_FILE = "haarcascade_frontalface_default.xml"
 cascade = cv2.CascadeClassifier(HAAR_FILE)
 
-client = boto3.client('rekognition')
+client = boto3.client('rekognition','ap-northeast-1')
 
 
 def rotate(point, theta_x, theta_y, theta_z):
@@ -33,12 +34,12 @@ def rotate(point, theta_x, theta_y, theta_z):
     return rot_point
 
 
-def detect_face_and_writeideo():
+def detect_face_and_writeideo(inputimg_path):
     #path = "target"
     #imageNum = 400
 
-    path = "IMG_2824"
-    imageNum = 6
+    path = inputimg_path
+    imageNum = len(os.listdir('images/%s'%(path)))
 
     # 1.download
     #s3 = boto3.resource('s3')
@@ -73,13 +74,14 @@ def detect_face_and_writeideo():
     
     # 4.wirte video
     
+    """
     cap = cv2.VideoCapture("video/%s.MOV"%(path))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS)/1)
     fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     writer = cv2.VideoWriter('%s_face.mp4'%(path), fmt, fps, (width, height))
-    
+    """
 
     for i in range(1, imageNum, 1):
         
@@ -141,32 +143,95 @@ def detect_face_and_writeideo():
             img = cv2.line(img, (center_x, center_y), (int(center_x+rot_y_axis[0]), int(center_y+rot_y_axis[1])), (0,255,0), 5)
         else:
             print(i)
-        writer.write(img)
-        cv2.imwrite(output_img,img)
+        #writer.write(img)
+        
+        #cv2.imwrite(output_img,img)
+        
 
-    writer.release()
+    #writer.release()
 
+def detect_face(img):
+    # 3.detect face
+    tmp_input_img = "images/tmp.png"
+    detect_responce = 'detect/tmp.json'
+    output_img = "images/output/tmp.png"
+   
+    copied_image = img.copy()
+    cv2.imwrite(tmp_input_img,copied_image)
+    with open(tmp_input_img, 'rb') as image:
+        response = client.detect_faces(Image={'Bytes': image.read()},Attributes=['ALL'])
+    with open(detect_responce, 'w') as f:
+        #print(response)
+        json.dump(response, f, indent=4)
+    #continue
+    
 
-def capture_camera():
+    # 4.draw face box
+    with open(detect_responce, 'r') as f:
+        response = json.loads(f.read())
+
+    if response["FaceDetails"]:       
+
+        width   = response["FaceDetails"][0]["BoundingBox"]["Width"]
+        height  = response["FaceDetails"][0]["BoundingBox"]["Height"]
+        left    = response["FaceDetails"][0]["BoundingBox"]["Left"]
+        top     = response["FaceDetails"][0]["BoundingBox"]["Top"]
+        roll    = response["FaceDetails"][0]["Pose"]["Roll"]
+        yaw     = response["FaceDetails"][0]["Pose"]["Yaw"]
+        pitch   = response["FaceDetails"][0]["Pose"]["Pitch"]
+        
+        img_height, img_width, _ = img.shape
+        width   = int(img_width * width)
+        height  = int(img_height * height)
+        left    = int(img_width * left)
+        top     = int(img_height * top)
+        img = cv2.rectangle(img, (left, top), (left+width, top+height), (255,0,0))
+
+        center_x    = int(left + width/2)
+        center_y    = int(top + height/2)
+        x_axis      = np.array([30, 0, 0])
+        y_axis      = np.array([ 0,30, 0])
+        z_axis      = np.array([ 0, 0,30])
+        rot_x_axis  = rotate(x_axis, roll, yaw, pitch)
+        rot_y_axis  = rotate(y_axis, roll, yaw, pitch)
+        rot_z_axis  = rotate(z_axis, roll, yaw, pitch)
+        
+        img = cv2.line(img, (center_x, center_y), (int(center_x+rot_x_axis[0]), int(center_y+rot_x_axis[1])), (0,0,255), 5)
+        img = cv2.line(img, (center_x, center_y), (int(center_x+rot_z_axis[0]), int(center_y+rot_z_axis[1])), (255,0,0), 5)
+        img = cv2.line(img, (center_x, center_y), (int(center_x+rot_y_axis[0]), int(center_y+rot_y_axis[1])), (0,255,0), 5)
+    else:
+        print("i")
+    #writer.write(img)
+    
+    #cv2.imwrite(output_img,img)
+        
+def capture_camera(output_path):
+    
     camera_id = 0 #0:incam 1:***
     cap = cv2.VideoCapture(camera_id)
 
+    i = 0
     while(True):
+        i+=1
         ret, frame = cap.read()
-        
-        face = cascade.detectMultiScale(frame)
-
-        print(face)
-
-        for x, y, w, h in face:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
-
-        cv2.imshow('Capture',frame)
+        if ret == False:
+            break
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
+        detect_face(frame)
+        
+        cv2.imshow("Input", frame) #ウィンドウに画像を表示する
+        
+        time.sleep(0.5)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+    
+if __name__=="__main__":
+    output_path  =  "IMG_2824"
+    capture_camera(output_path = output_path)
 
-        break
-
-if __name__=="__main__":    
-    capture_camera()
